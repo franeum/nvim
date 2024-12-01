e7fea4234d1e3abd702a4effcc1a615bb3e77f1a
franeum <elettrochuck@gmail.com>
2024-12-01 02:13:29 +0100

    update: now SC snippets are suggested

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ Plug('L3MON4D3/LuaSnip', { ['tag'] = 'v2.*', ['do'] = 'make install_jsregexp' })
 Plug("saadparwaiz1/cmp_luasnip")
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
+Plug('rafamadriz/friendly-snippets')
 
 vim.call('plug#end')
 
@@ -42,3 +43,47 @@ vim.cmd [[
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
 ]]
+
+
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  },
+  formatting = {
+    fields = { 'abbr', 'kind', 'menu' },
+    format = function(entry, vim_item)
+      vim_item.kind = string.format('%s', vim_item.kind)
+      vim_item.menu = ({
+        luasnip = '[Snip]',
+        nvim_lsp = '[LSP]',
+        buffer = '[Buffer]',
+        path = '[Path]',
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+})
+
+-- Abilita il completamento per gli snippet
+require'cmp'.setup {
+  sources = {
+    { name = 'luasnip' },
+  },
+}
